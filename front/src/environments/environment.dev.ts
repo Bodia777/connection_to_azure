ae09909a7ff57ce2ecee3d15705b5fa242645d3f
Bohdan Kmet <78738798+BohdanKmetIsraelIt@users.noreply.github.com>
2026-04-18 20:18:04 +0300

    structure changes

---
+++ front/src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  defaultLocale: 'en',
+  baseUrl: 'https:backend-app.happybay-94528ba9.westeurope.azurecontainerapps.io',
+  frontUrl: 'https://frontend-app.happybay-94528ba9.westeurope.azurecontainerapps.io',
+};
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,8 +3,4 @@ export const environment = {
   defaultLocale: 'en',
   baseUrl: 'http://localhost:3000',
   frontUrl: 'http://localhost:4200',
-  fileChunkSize: 1024 * 1024 * 5,
-  keycloakUrl: 'http://keycloak.localhost:9090',
-  keycloakRealm: 'EY',
-  keycloakClientId: 'EYApi',
 };
