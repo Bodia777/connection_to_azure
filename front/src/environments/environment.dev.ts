87155c344936f8928872aedd5b575a3df862fb91
Bohdan Kmet <78738798+BohdanKmetIsraelIt@users.noreply.github.com>
2026-04-26 20:37:34 +0300

    restart deploy

--- front/src/environments/environment.dev.ts
+++ front/src/environments/environment.dev.ts
@@ -4,3 +4,4 @@ export const environment = {
   baseUrl: 'https:backend-app.happybay-94528ba9.westeurope.azurecontainerapps.io',
   frontUrl: 'https://frontend-app.happybay-94528ba9.westeurope.azurecontainerapps.io',
 };
+
